8d20396551d4637f7168a5dc6a28d3be8b828791
RSlover521 <wulance58@gmail.com>
2025-07-18 13:25:57 -0500

    added demo work

---
+++ Jacob_Demo_Work/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Jacob_Demo_Work {
+}
---
+++ Jacob_Demo_Work/src/wu/MAIN/Xinchen.java
@@ -0,0 +1,15 @@
+package wu.MAIN;
+
+public class Xinchen{
+	public static void main(String[] args) {
+		// %: modulo (mod)
+		int n = 3;
+		// if n modulo 2 is exactly 1
+		if(n % 2 == 1) { // checks if n is odd
+			System.out.println("n is odd");
+		}
+		else { // not odd, else it is even
+			System.out.println("n is even");
+		}
+	}
+}
