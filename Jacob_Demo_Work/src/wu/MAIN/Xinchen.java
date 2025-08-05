fab0e9ba83310ecaecd0aab4872f3c67628ff0d3
RSlover521 <wulance58@gmail.com>
2025-08-05 18:05:42 -0500

    what

--- Jacob_Demo_Work/src/wu/MAIN/Xinchen.java
+++ Jacob_Demo_Work/src/wu/MAIN/Xinchen.java
@@ -1,11 +1,5 @@
 package wu.MAIN;
 
 public class Xinchen{
-	public static void main(String[] args) {
-		// pro tip: always put your task before doing your homework or other projects
-		// Homework 4: print from 20 to 0
-		for (int i = 20; i >= 0; i--) {
-			System.out.println(i);
-		}
-	}
+	
 }
