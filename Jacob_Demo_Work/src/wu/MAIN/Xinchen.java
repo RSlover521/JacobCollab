f9b0d9b4d2fea3cefc8dc253e05117f4b9673017
RSlover521 <wulance58@gmail.com>
2025-09-05 17:16:17 -0500

    lesson 14 hw answer

--- Jacob_Demo_Work/src/wu/MAIN/Xinchen.java
+++ Jacob_Demo_Work/src/wu/MAIN/Xinchen.java
@@ -1,7 +1,22 @@
 package wu.MAIN;
 
-public class Xinchen{
+import java.util.Scanner;
+
+public class Xinchen {
 	public static void main(String[] args) {
+		// Creating an instance (an object) of scanner
+		Scanner scan = new Scanner(System.in);
 		
+		// prints something that prompts the user to enter the number of quarter
+		System.out.print("Enter your number of quarters: ");
+		
+		// detect what integer the user inputted
+		int quarter = scan.nextInt();
+		
+		// calculates how many total cents in x number of quarters
+		int cents = quarter * 25;
+		
+		// prints out the total cents
+		System.out.println("The number of quarters you entered is equal to " + cents + " cents");
 	}
 }
