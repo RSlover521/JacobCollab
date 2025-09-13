410d9516835508d22894e9d8aa0e92090a5f9b8c
RSlover521 <wulance58@gmail.com>
2025-09-12 19:31:35 -0500

    added task: print monkey

--- Jacob_Demo_Work/src/wu/MAIN/Xinchen.java
+++ Jacob_Demo_Work/src/wu/MAIN/Xinchen.java
@@ -2,14 +2,7 @@ package wu.MAIN;
 
 public class Xinchen {
 	public static void main(String[] args) {
-		// print i love lebron
-		System.out.println("I ate lebroonickers");
-		
-		// print hello world
-		System.out.println("hello world");
-	
-		// print "the elephant dog"
-		System.out.println("the elephant doge");
+		// print "monkey"
 		
 		
 	}
