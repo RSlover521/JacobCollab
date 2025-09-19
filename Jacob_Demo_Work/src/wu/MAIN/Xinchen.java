bc0d8aff1806e498e00eb127bdcb57c059ae64d9
RSlover521 <wulance58@gmail.com>
2025-09-19 17:33:46 -0500

    added new task

--- Jacob_Demo_Work/src/wu/MAIN/Xinchen.java
+++ Jacob_Demo_Work/src/wu/MAIN/Xinchen.java
@@ -3,7 +3,11 @@ package wu.MAIN;
 public class Xinchen {
 	public static void main(String[] args) {
 		// print "monkey"
-	System.out.println("Monkey");	
+		System.out.println("Monkey");	
+		
+		
+		// print "nigger"
+		
 		
 	}
 }
