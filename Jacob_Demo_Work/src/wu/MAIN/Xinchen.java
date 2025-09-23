8f136ea75e06a00b9503cd401c31683e77fc8919
RSlover521 <wulance58@gmail.com>
2025-09-23 17:34:31 -0500

    67

--- Jacob_Demo_Work/src/wu/MAIN/Xinchen.java
+++ Jacob_Demo_Work/src/wu/MAIN/Xinchen.java
@@ -2,12 +2,6 @@ package wu.MAIN;
 
 public class Xinchen {
 	public static void main(String[] args) {
-		// print "monkey"
-		System.out.println("Monkey");	
-		
-		
-		// print "nigger"
-		System.out.println("nigger");
-		
+		System.out.println("67");
 	}
 }
