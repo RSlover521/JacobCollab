b76afd6c34082d7dd0592a66ffdb54e5e5a183f7
jacobhacks900 <jli03280328@gmail.com>
2025-09-23 17:29:03 -0500

    SiixSavan

--- Jacob_Demo_Work/src/wu/MAIN/Xinchen.java
+++ Jacob_Demo_Work/src/wu/MAIN/Xinchen.java
@@ -2,12 +2,8 @@ package wu.MAIN;
 
 public class Xinchen {
 	public static void main(String[] args) {
-		// print "monkey"
-		System.out.println("Monkey");	
-		
-		
-		// print "nigger"
-		System.out.println("nigger");
+		//write a line of c0de that prints "Siix-Savan"
+		System.out.println("Siix-Savan");
 		
 	}
 }
