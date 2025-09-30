c8e0c6c000d71654a892628ffde9a5767399a837
jacobhacks900 <jli03280328@gmail.com>
2025-09-30 17:52:42 -0500

    scan

--- Jacob_Demo_Work/src/wu/MAIN/Xinchen.java
+++ Jacob_Demo_Work/src/wu/MAIN/Xinchen.java
@@ -1,8 +1,10 @@
 package wu.MAIN;
 
+import java.util.Scanner;
+
 public class Xinchen {
 	public static void main(String[] args) {
 		// write a line of code that creates a Scanner object and call it scan (do you remember how to do it?)
-		
+		Scanner scan = new Scanner (System.in );
 	}
 }
